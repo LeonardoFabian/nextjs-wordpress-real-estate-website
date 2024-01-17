b2b47e89a7c0b318bdaa4d2eda44e79c997ca005
Leonardo Fabian <ramonlfabian@gmail.com>
2024-01-17 14:58:07 -0400

    last commit

--- context/page.js
+++ context/page.js
@@ -2,6 +2,7 @@ import { createContext, useContext } from "react";
 
 const PageContext = createContext();
 
+// page wrapper
 export const PageWrapper = ({value, children}) => {
     return <PageContext.Provider value={value}>{children}</PageContext.Provider>;
 };
