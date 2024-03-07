427779447f9a5fa9a8501a83a46fe30b1646ac8c
Leonardo Fabian <ramonlfabian@gmail.com>
2024-03-07 04:00:30 -0400

    return null if menuItem destination no exists

--- utils/mapMainMenuItems.js
+++ utils/mapMainMenuItems.js
@@ -3,7 +3,7 @@ import {v4 as uuid} from 'uuid'
 export const mapMainMenuItems = (menuItems) => {
     return (menuItems || []).map(menuItem => ({
         id: uuid(),
-        destination: menuItem.menuItem.destination.uri || "",
+        destination: menuItem.menuItem.destination.uri || null,
         label: menuItem.menuItem.label,
         subMenuItems: (menuItem.items || []).map((subMenuItem) => ({
             id: uuid(),
